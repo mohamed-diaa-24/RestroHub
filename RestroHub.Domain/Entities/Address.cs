488499efb05cd30ae819e33564e6a86110d77c02
Mohamed Diaa <mohamed.diaa.elsayed0@gmail.com>
2025-10-10 11:14:06 +0300

    (feat) : Add domain entities and project reference to API

---
+++ RestroHub.Domain/Entities/Address.cs
@@ -0,0 +1,7 @@
+﻿namespace RestroHub.Domain.Entities;
+public class Address
+{
+    public string? City { get; set; }
+    public string? Street { get; set; }
+    public string? PostalCode { get; set; }
+}
