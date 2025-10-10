27ca777b2a63d852bbf72c71c3092b5268f11f51
Mohamed Diaa <mohamed.diaa.elsayed0@gmail.com>
2025-10-10 12:05:09 +0300

    (feat) : Add EF Core schema for Restaurants and Dishes

--- RestroHub.Domain/Entities/Dish.cs
+++ RestroHub.Domain/Entities/Dish.cs
@@ -5,4 +5,5 @@ public class Dish
     public string Name { get; set; } = default!;
     public string Description { get; set; } = default!;
     public decimal Price { get; set; }
+    public int RestaurantId { get; set; }
 }
---
+++ RestroHub.Infrastrcture/Persistence/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestroHub.Domain.Entities;
+
+namespace RestroHub.Infrastrcture.Persistence;
+
+internal class AppDbContext : DbContext
+{
+    internal DbSet<Restaurant> Restaurants { get; set; }
+    internal DbSet<Dish> Dishes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=.;Database=RestoHubDb;Trusted_Connection=true;TrustServerCertificate=True");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Restaurant>()
+            .OwnsOne(r=>r.Address);
+
+        modelBuilder.Entity<Restaurant>()
+            .HasMany(r => r.Dishes)
+            .WithOne()
+            .HasForeignKey(d=>d.RestaurantId);
+    }
+}
