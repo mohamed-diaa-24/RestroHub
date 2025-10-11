f3463910f825ffe90c07d4e8066fd0395552780a
Mohamed Diaa <mohamed.diaa.elsayed0@gmail.com>
2025-10-12 00:09:48 +0300

    (fix) : Fix Infrastructure typo

--- RestroHub.Infrastrcture/Migrations/AppDbContextModelSnapshot.cs
+++ RestroHub.Infrastrcture/Migrations/AppDbContextModelSnapshot.cs
@@ -3,11 +3,11 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using RestroHub.Infrastrcture.Persistence;
+using RestroHub.Infrastructure.Persistence;
 
 #nullable disable
 
-namespace RestroHub.Infrastrcture.Migrations
+namespace RestroHub.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
     partial class AppDbContextModelSnapshot : ModelSnapshot
