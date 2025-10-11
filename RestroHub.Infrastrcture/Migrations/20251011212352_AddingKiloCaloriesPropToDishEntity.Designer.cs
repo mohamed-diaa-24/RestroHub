0f033197c1a4b22f983555bffd67ef2311afad9e
Mohamed Diaa <mohamed.diaa.elsayed0@gmail.com>
2025-10-12 00:26:36 +0300

    (feat) : Add KiloCalories property to Dish entity

--- RestroHub.Infrastrcture/Migrations/20251011212352_AddingKiloCaloriesPropToDishEntity.Designer.cs
+++ RestroHub.Infrastrcture/Migrations/20251011212352_AddingKiloCaloriesPropToDishEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RestroHub.Infrastructure.Persistence;
 
@@ -11,9 +12,11 @@ using RestroHub.Infrastructure.Persistence;
 namespace RestroHub.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251011212352_AddingKiloCaloriesPropToDishEntity")]
+    partial class AddingKiloCaloriesPropToDishEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RestroHub.Infrastrcture/Migrations/20251011212352_AddingKiloCaloriesPropToDishEntity.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestroHub.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingKiloCaloriesPropToDishEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "KiloCalories",
+                table: "Dishes",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "KiloCalories",
+                table: "Dishes");
+        }
+    }
+}
