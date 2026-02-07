83bf0104a83841a62ee9041fdde7897736326b7f
Mohamed Diaa <mohamed.diaa.elsayed0@gmail.com>
2026-02-08 00:22:19 +0200

    (feat) : Add Serilog conig to appsettings.Development.json

--- RestroHub.API/Program.cs
+++ RestroHub.API/Program.cs
@@ -2,23 +2,22 @@ using RestroHub.Infrastructure.Extensions;
 using RestroHub.Infrastructure.Seeders;
 using RestroHub.Application.Extensions;
 using Serilog;
-using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 builder.Services.AddInfrastructure(builder.Configuration);
+
 builder.Services.AddApplication(builder.Configuration);
+
 builder.Host.UseSerilog((context, configuration) =>
 {
-    configuration
-        .MinimumLevel.Override("Microsoft",LogEventLevel.Warning)
-        .MinimumLevel.Override("Microsoft.EntityFrameworkCore",LogEventLevel.Information)
-        .WriteTo
-        .Console(outputTemplate:"[{Timestamp:dd-MM HH:mm:ss} {Level:u3}] |{SourceContext}| {NewLine} {Message:lj}{NewLine}{Exception}");
+    configuration.ReadFrom.Configuration(context.Configuration);
 });
+
 var app = builder.Build();
 
 
